09a19fd78c29b32971fbe08a4c7f26712ee4883d
Ahmad Yasir <51242252+yasirahmad62@users.noreply.github.com>
2020-05-21 17:40:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImage= "dice"+randomNumber1+".png";
+var randomImageSource= "images/"+randomImage;
+var image1 = document.querySelectorAll("img")[0];//top tap in for first image index 0
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2= "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+
+if (randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="Player1 WINS 🚩"
+}
+else if (randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="🚩 Player2 WINS "
+}
+else{
+  document.querySelector("h1").innerHTML="Draw 😹"
+}
